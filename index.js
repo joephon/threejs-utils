2d8d6c770bd922e3ba1646d6b9a8d6f5adac3093
joephon <306519779@qq.com>
2016-10-17 21:46:43 +0800

    added all controls

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ var Stats = require('./lib/stats.min.js')
 var THREE = require('./lib/three.min.js')
 var dat = require('./lib/dat.gui.min.js')
 var DeviceOrientationControls = require('./lib/DeviceOrientationControls.js')
+var DragControls = require('./lib/DragControls.js')
+var EditorControls = require('./lib/EditorControls.js')
+var FirstPersonControls = require('./lib/FirstPersonControls.js')
+var FlyControls = require('./lib/FlyControls.js')
+var MouseControls = require('./lib/MouseControls.js')
+var OrbitControls = require('./lib/OrbitControls.js')
+var OrthographicTrackballControls = require('./lib/OrthographicTrackballControls.js')
+var PointerLockControls = require('./lib/PointerLockControls.js')
 var TrackballControls = require('./lib/TrackballControls.js')
+var TransformControls = require('./lib/TransformControls.js')
+var VRControls = require('./lib/VRControls.js')
+
 module.exports = {
   Stats: Stats,
   THREE: THREE,
   dat: dat,
   DeviceOrientationControls,
+  DragControls,
+  EditorControls,
+  FirstPersonControls,
+  FlyControls,
+  MouseControls,
+  OrbitControls,
+  OrthographicTrackballControls,
+  PointerLockControls,
   TrackballControls,
+  TransformControls,
+  VRControls,
 }
