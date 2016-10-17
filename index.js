e7c9cf885348c26961536045f9d9f59f6fa57251
joephon <306519779@qq.com>
2016-10-17 21:29:18 +0800

    fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Stats = require('./lib/stats.min.js')
 var THREE = require('./lib/three.min.js')
 var dat = require('./lib/dat.gui.min.js')
-var DeviceOrientationControls = require('./DeviceOrientationControls.js')
-var TrackballControls = require('./TrackballControls.js')
+var DeviceOrientationControls = require('./lib/DeviceOrientationControls.js')
+var TrackballControls = require('./lib/TrackballControls.js')
 module.exports = {
   Stats: Stats,
   THREE: THREE,
