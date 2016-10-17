a7b7d83da100d84e7f825237d3fe945b699db1f5
joephon <306519779@qq.com>
2016-10-17 15:03:16 +0800

    added controls

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Stats = require('./lib/stats.min.js')
 var THREE = require('./lib/three.min.js')
 var dat = require('./lib/dat.gui.min.js')
+var TrackBallControls = require('./lib/TrackBallControls.js')
+THREE.TrackBallControls = TrackBallControls
 
 module.exports = {
   Stats: Stats,
