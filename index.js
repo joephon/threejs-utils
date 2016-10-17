ff34b9f4c76416508c16bf45b770f2ee50d8779b
joephon <306519779@qq.com>
2016-10-17 21:27:43 +0800

    added controls

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var Stats = require('./lib/stats.min.js')
 var THREE = require('./lib/three.min.js')
 var dat = require('./lib/dat.gui.min.js')
+var DeviceOrientationControls = require('./DeviceOrientationControls.js')
+var TrackballControls = require('./TrackballControls.js')
 module.exports = {
   Stats: Stats,
   THREE: THREE,
-  dat: dat
+  dat: dat,
+  DeviceOrientationControls,
+  TrackballControls,
 }
